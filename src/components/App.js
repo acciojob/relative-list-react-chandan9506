77e3e49f382ac308d177fd237e759b9df35d344a
Chandan <chandan.ckmodi@gmail.com>
2024-08-25 21:02:24 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const App = () => {
+
+  const relatives =["chandan","pawan","sandeep","miku","ansh"];
+
   return (
     <div id="main">
                {/* Do not remove the main div */}
+              
+
+             <ol key="relativeList">
+              {relatives.map((item,index) =>
+                  <li key={`relativeListItem${index + 1}`}>{item}</li>
+              )}
+             
+             </ol>
     </div>
   )
 }
