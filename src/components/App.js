28c5dcceb8ae480a3c3cbe30552a4ef39361495c
Chandan <chandan.ckmodi@gmail.com>
2024-09-02 09:19:16 +0530

    1.4

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,14 @@
 import React, { useState } from 'react'
 
 const App = () => {
-
-  
-
-  const [relatives] = useState(["chandan","pawan","sandeep","miku","ansh"]);
-
+  const[arr] = useState(["Satish", "Ankita", "Amit", "Chetan", "Vijay", "Rahul"]);
   return (
     <div id="main">
-               {/* Do not remove the main div */}
-              
-
-             <ol key="relativeList" id='realativeList'>
-              {relatives.map((item,index) =>
-                  <li key={`relativeListItem${index + 1}`}  id={'relativeListItem'+(index+1)}>{item}</li>
-              )}
-             
-             </ol>
+               <ol key="relativeList" id='relativeList'>
+                {
+                  arr.map((item, index) => <li key={'relativeListItem'+(index+1)} id={'relativeListItem'+(index+1)}>{item}</li>)
+                }
+               </ol>
     </div>
   )
 }
